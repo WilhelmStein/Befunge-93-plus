7c12d0868e02d4c6a6b2a24eaa5ce9ad065dd3dc
WilhelmStein <johnfreeman16561@gmail.com>
2019-11-15 18:24:10 +0200

    counter that alters playing field test

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ make clean && cereal --rebuild
 
 for filename in ./tests/*.bf; do
     test_output=${filename/.bf/.out}
-    diff --text <(./bin/befunge93.exe $filename) <(./Befunge-93/bin/bef $filename) > ${test_output}
+    diff -y --text <(./bin/befunge93.exe $filename) <(./Befunge-93/bin/bef $filename) > ${test_output}
 done
